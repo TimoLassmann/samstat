a438ba2237b20133adb17f453ad3257949a24c5b
lassmann <lassmann@gmail.com>
2014-06-12 10:35:30 +0900

    Initial Commit

---
+++ samstat2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  samstat2
+//
+//  Created by lassmann on 6/12/14.
+//  Copyright (c) 2014 lassmann. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+

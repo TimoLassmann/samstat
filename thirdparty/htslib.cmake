4aa359173eca41192dc5015496f1f6d04642fe81
Timo Lassmann <timo.lassmann@telethonkids.org.au>
2022-06-17 00:06:24 +0800

    Removed tarball

--- thirdparty/htslib.cmake
+++ thirdparty/htslib.cmake
@@ -6,7 +6,7 @@ include(ExternalProject)
 
 ExternalProject_Add(htslib
         BUILD_IN_SOURCE 1        
-        URL  ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/htslib-1.15.1.tar.bz2 #[https://github.com/samtools/htslib/releases/download/1.15.1/htslib-1.15.1.tar.bz2]
+        URL https://github.com/samtools/htslib/releases/download/1.15.1/htslib-1.15.1.tar.bz2
         PREFIX ${htslib_PREFIX}
         UPDATE_COMMAND ""
         CONFIGURE_COMMAND autoreconf -i && ./configure --prefix=${CMAKE_BINARY_DIR}/htslib
